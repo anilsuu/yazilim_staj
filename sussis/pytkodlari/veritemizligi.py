0c234ff6fec30dffdfe384a2d4c5bf77f7e79580
nilsu <studiewnils7@gmail.com>
2026-08-24 09:57:12 +0300

    'Sulfate' sütununda 783 eksik veri olduğu için medyan ile doldurmak manipüle edecektir .Bu yüzden kNN imputation yaptım.

--- sussis/pytkodlari/veritemizligi.py
+++ sussis/pytkodlari/veritemizligi.py
@@ -135,7 +135,22 @@ print("-----------------------------------------------------")
 
 hesaplamainfo("Sulfate")
 
+# medyanladoldurma("Sulfate")
 
+df_knn=df.filter(["Sulfate","ph","Hardness"],axis=1).copy()
+
+scaler=MinMaxScaler()
+df_knn = pd.DataFrame(scaler.fit_transform(df_knn), columns = df_knn.columns)
+
+# Define KNN imputer and fill missing values
+knn_imputer = KNNImputer(n_neighbors=5, weights='distance')
+df_knn_imputed = pd.DataFrame(knn_imputer.fit_transform(df_knn), columns=df_knn.columns)
+
+print(df_knn_imputed)
+df_imputed = pd.DataFrame(scaler.inverse_transform(df_knn_imputed), columns=df_knn.columns)
+
+# 4. Sadece doldurulmuş Sulfate sütununu, asıl df'mizin içine aktarıyoruz
+df["Sulfate"] = df_imputed["Sulfate"]
 
 nansızsütuninfo("Sulfate")
 
