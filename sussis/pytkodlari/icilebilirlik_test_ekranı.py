726150ab1c89d2cb52513b5617b37ed0f53e9c48
nilsu <studiewnils7@gmail.com>
2026-09-02 10:08:00 +0300

    Girilen değerlerin scaler edilme işlemlerinin kodları, modelden tahmini prediction alma.

--- sussis/pytkodlari/icilebilirlik_test_ekranı.py
+++ sussis/pytkodlari/icilebilirlik_test_ekranı.py
@@ -128,17 +128,38 @@ with sag_kolon:
 #         st.markdown("#### GİRİŞ PARAMETRELERİ")
 #         st.markdown("<hr style='border:1px solid white'>", unsafe_allow_html=True)
 
-#Girilecek parametrelerin .input olarak veri tipini belirterek
-        ph = float(st.number_input("💧 pH Seviyesi", step=1.0, min_value=0.22749905, max_value=14.0, help="0-14 değerleri arasında olmalıdır.")) 
-        Hardness = float(st.number_input("🪨 (Hardness) Sertlik ", step=1.0, min_value=73.49223369, help="Sertlik(Hardness) değerini giriniz."))
-        Solids = float(st.number_input("🧊 Solids (Katılar)", step=1.0, min_value=320.9426113, help="Solids(Katılar) değerini giriniz."))
-        Chloramines = float(st.number_input("🧪 Chloramines(Kloramin)", step=1.0, min_value=1.390870905, help="Chloramines(Kloramin) değerini giriniz."))
-        Sulfate = float(st.number_input("🟣 Sulfate(Sülfat)", step=1.0, min_value=129.0, help="Sulfate(Sülfat) değerini giriniz."))
-        Conductivity = float(st.number_input("⚡ Conductivity(İletkenlik)", step=1.0, min_value=201.6197368, help="20°C Conductivity (İletkenlik) değerini giriniz."))
-        Organic_carbon = float(st.number_input("💬 Organic_carbon (Organik karbon)", step=1.0, min_value=2.2, help="Organic_carbon (Organik karbon) değerini giriniz."))
-        Trihalomethanes = float(st.number_input("🟠 Trihalomethanes(Trihalometanlar)", step=1.0, min_value=8.577012933, help="Trihalomethanes(Trihalometanlar) değerini giriniz."))
-        Turbidity = float(st.number_input("🌀 Turbidity(Bulanıklık)", step=1.0, min_value=1.45, help="Turbidity(Bulanıklık) değerini giriniz."))
 
+
+#Girilecek parametrelerin .input olarak veri tipini belirterek,help kutusundaki mesaj açılması.
+        ph = float(st.number_input("💧 pH Seviyesi", step=1.0, min_value=0.22749905, max_value=14.0, help="ph bilgisi 0-14 değerleri arasında olmalıdır.")) 
+        Hardness = float(st.number_input("🪨 (Hardness) Sertlik ", step=1.0, min_value=73.49223369, help="Sertlik(Hardness):Suyun bir yüzeye temas etmeye karşı gösterdiği dirençtir."))
+        Solids = float(st.number_input("🧊 Solids (Katılar)", step=1.0, min_value=320.9426113, help="Solids(Katılar):Suyun içinde çözünmüş halde bulunan mineral, tuz ve iyonların toplam miktarını ifade eder."))
+        Chloramines = float(st.number_input("🧪 Chloramines(Kloramin)", step=1.0, min_value=1.390870905, help="Chloramines(Kloramin):Suyun dezenfeksiyon aşamasında klor kullanılınca oluşur"))
+        Sulfate = float(st.number_input("🟣 Sulfate(Sülfat)", step=1.0, min_value=129.0, help="Sulfate(Sülfat):Suyun kalitesini değerlendirmek için ölçülür."))
+        Conductivity = float(st.number_input("⚡ Conductivity(İletkenlik)", step=1.0, min_value=201.6197368, help="20°C Conductivity (İletkenlik) genellikle 50-500 değerleri arasında olur."))
+        Organic_carbon = float(st.number_input("💬 Organic_carbon (Organik karbon)", step=1.0, min_value=2.2, help="Organic_carbon (Organik karbon) değerini giriniz."))
+        Trihalomethanes = float(st.number_input("🟠 Trihalomethanes(Trihalometanlar)", step=1.0, min_value=8.577012933, help="Trihalomethanes(Trihalometanlar) için sınır değer, 2005 yılından beri gereği 100 µg/L olarak belirlenmiştir."))
+        Turbidity = float(st.number_input("🌀 Turbidity(Bulanıklık)", step=1.0, min_value=1.45, help="Turbidity(Bulanıklık):Suyun içindeki askıda katı maddelerin ışığı dağıtmasıyla suyun berraklığının azalmasına denir."))
+
+if st.button("Analiz Et 🚀", key="yeni_test_butonu"):
+    #Arayüzden gelen tüm değerler Pandas ile DataFrame'e dönüştürülüyor.
+    #Değişken isimleri (Hardness,Solids vs.) yukarıda tanımlandığı gibi DataFrame ekledim.
+    
+    input_data=pd.DataFrame({
+        "ph": [ph],
+        "Hardness": [Hardness],
+        "Solids":[Solids],
+        "Chloramines":[Chloramines],
+        "Sulfate":[Sulfate],
+        "Conductivity":[Conductivity],
+        "Organic_carbon":[Organic_carbon],
+        "Trihalomethanes":[Trihalomethanes],
+        "Turbidity":[Turbidity]
+        })
+    try:
+        # Girilen verileri , eğitilmiş scaler ile ölçeklendirme
+        input_scaled=scaler.transform(input_data)
+    
 #         if st.button("Analiz Et 🚀",key="yeni_test_butonu"):
 #             # 1. Arayüzden gelen tüm değerler Pandas DataFrame'e dönüştürülüyor
 #             # Değişken isimleri (Hardness, Solids vs.) yukarıda tanımlandığı gibi dataframe eklendi.
